7f6cf130653681d6b6d0314a1045f88a32ca34a4
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-02-27 01:45:01 +0300

    jms initial configuration.

---
+++ inventory-service/src/main/java/com/muhammedtopgul/inventoryservice/configuration/JmsConfig.java
@@ -0,0 +1,24 @@
+package com.muhammedtopgul.inventoryservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author muhammed-topgul
+ * @since 27.02.2022 01:34
+ */
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
