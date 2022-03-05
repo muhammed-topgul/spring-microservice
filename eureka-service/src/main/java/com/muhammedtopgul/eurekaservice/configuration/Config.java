9bc8c3fd8e1a306925b213d4ac6c9e67327b9b5e
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-03-06 00:55:12 +0300

    eureka-service application created.

---
+++ eureka-service/src/main/java/com/muhammedtopgul/eurekaservice/configuration/Config.java
@@ -0,0 +1,14 @@
+package com.muhammedtopgul.eurekaservice.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author muhammed-topgul
+ * @since 06.03.2022 00:52
+ */
+
+@Configuration
+@EnableEurekaServer
+public class Config {
+}
