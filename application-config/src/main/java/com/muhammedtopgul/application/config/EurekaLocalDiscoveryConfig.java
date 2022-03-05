c2fc99c6956a349e01a1fa5dc7d0b5982ed7edc3
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-03-06 01:11:40 +0300

    eureka discovery client config in application-bom.

---
+++ application-config/src/main/java/com/muhammedtopgul/application/config/EurekaLocalDiscoveryConfig.java
@@ -0,0 +1,16 @@
+package com.muhammedtopgul.application.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author muhammed-topgul
+ * @since 06.03.2022 01:02
+ */
+
+@Profile("eureka-local-discovery")
+@Configuration
+@EnableDiscoveryClient
+public class EurekaLocalDiscoveryConfig {
+}
